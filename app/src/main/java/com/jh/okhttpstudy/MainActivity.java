e279308d868f8622087c1c58c2d414d891715aa2
jinhuizxc <1004260403@qq.com>
2019-02-13 15:18:19 +0800

    Initial commit

---
+++ app/src/main/java/com/jh/okhttpstudy/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jh.okhttpstudy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * OKHttp的学习
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
